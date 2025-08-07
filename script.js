7c5f72e9d52766ccbecdd5469a1450b42888d000
LaLeggenda <49498244+LaLeggenda@users.noreply.github.com>
2025-08-07 17:38:26 +0200

    Add files via upload
    refactored

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// === Hamburger Menu Toggle ===
+function toggleMenu() {
+    var nav = document.querySelector('.nav-items');
+    var icon = document.querySelector('.hamburger');
+    nav.classList.toggle('show');
+    icon.classList.toggle('active');
+}
